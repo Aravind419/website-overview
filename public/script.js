a440e6bedda72d22c7d83d6b6bffabe23e227035
Aravind419 <aravindaravind19720@gmail.com>
2024-12-02 19:39:06 +0530

    se

--- public/script.js
+++ public/script.js
@@ -24,7 +24,6 @@ document.addEventListener("DOMContentLoaded", () => {
         headers: {
           "Content-Type": "application/json",
         },
-        credentials: "include",
         body: JSON.stringify({ url }),
       });
 
